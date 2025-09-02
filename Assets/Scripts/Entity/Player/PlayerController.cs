6b1397d103851a00cdfd4494f57c8a06e11673bb
Winebug <ydjoo0410@gmail.com>
2025-09-02 20:54:57 +0900

    [feat] Dash 기능 추가 및 카메라 수정
    [feat] Dash 기능 추가 및 카메라 수정

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections; // Coroutine을 사용하기 위해 필요합니다.
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,12 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     private Player _player;
     private PlayerInput _playerinputs;
+    private PlayerAnimator _playerAnimator;
     private Vector2 moveInput;
+    private Rigidbody _rb;
+    private Camera _mainCamera;
+
+    [Header("Rotation Settings")]
+    public float rotationSpeed = 15f;
+
+    [Header("Dash Settings")]
+    [Tooltip("대쉬 지속시간 (초 단위)")]
+    public float dashDuration = 0.5f;
+
+    private bool isDashing = false; 
 
     private void Awake()
     {
         _player = GetComponent<Player>();
         _playerinputs = new PlayerInput();
+        _playerAnimator = GetComponent<PlayerAnimator>();
+        _rb = GetComponent<Rigidbody>();
+        _mainCamera = Camera.main;
     }
 
     private void OnEnable()
@@ -28,14 +44,52 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         moveInput = _playerinputs.Player.Move.ReadValue<Vector2>();
-
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
-
-        transform.Translate(movement * _player.moveSpeed * Time.deltaTime);
+        _playerAnimator.SetMoveAnimation(moveInput);
+        RotateTowardsMouse();
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
+        _rb.velocity = movement * _player.moveSpeed;
+    }
+
+    private void RotateTowardsMouse()
+    {
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 lookPoint = ray.GetPoint(distance);
+            Vector3 lookDirection = lookPoint - transform.position;
+            lookDirection.y = 1f;
+
+            if (lookDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(lookDirection);
+            }
+        }
+    }
+    #region Dash 함수
     private void OnDash(InputAction.CallbackContext context)
     {
-        Debug.Log("Dash!");
+        if (isDashing)
+        {
+            return;
+        }
+        _playerAnimator.PlayDashAnimation();
+
+        StartCoroutine(DashCooldownCoroutine());
     }
+
+    private IEnumerator DashCooldownCoroutine()
+    {
+        isDashing = true;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
+    }
+    #endregion
 }

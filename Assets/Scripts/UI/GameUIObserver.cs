f18528a92e7bf4f02fbc3713a4395e9e9a53fc06
Hira7388 <mine7388@gmail.com>
2025-09-05 11:36:26 +0900

    [Feat] UI 옵저버 생성 및 프리팹화

---
+++ Assets/Scripts/UI/GameUIObserver.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIObserver : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    void OnEnable()
+    {
+        if (_player != null)
+        {
+            _player.OnLevelChanged += HandlePlayerLevelUp;
+        }
+    }
+
+
+    void OnDisable()
+    {
+        if (_player != null)
+        {
+            _player.OnLevelChanged -= HandlePlayerLevelUp;
+        }
+    }
+
+    // 각 이벤트에 대한 처리기 메소드들입니다.
+    // 플레이어 레벨업 메서드
+    private void HandlePlayerLevelUp()
+    {
+        Debug.Log("GameUIObserver: 플레이어 레벨업 감지! 레벨업 UI를 요청합니다.");
+        UIManager.Instance.OpenUI<UISelectItem>();
+    }
+
+    // 게임 종료 메서드
+    private void HandleGameEnd()
+    {
+        Debug.Log("GameUIObserver: 게임 종료를 감지! 결과창 UI를 요청합니다.");
+        UIManager.Instance.OpenUI<UIResult>();
+    }
+}
--- Assets/Scripts/UI/UIResult.cs
+++ Assets/Scripts/UI/UIResult.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIResult : MonoBehaviour
+public class UIResult : UIBase
 {
     // Start is called before the first frame update
     void Start()

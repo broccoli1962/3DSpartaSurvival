110e88f8e0549023e9854d846004f71c47045485
Hira7388 <mine7388@gmail.com>
2025-09-05 15:12:53 +0900

    [Fix] UI 메인 매뉴 상점 버튼 삭제 수정

--- Assets/Scripts/UI/UITitle.cs
+++ Assets/Scripts/UI/UITitle.cs
@@ -8,7 +8,6 @@ public class UITitle : UIBase
 {
     [field: SerializeField] public Button btnGameStart { get; private set; }
     [field: SerializeField] public Button btnOption { get; private set; }
-    [field: SerializeField] public Button btnShop { get; private set; }
 
 
     // Title UI를 호출했을 때 초기화 동작을 진행하는 메서드
@@ -17,7 +16,6 @@ public class UITitle : UIBase
         base.OnOpen();
         btnGameStart?.onClick.AddListener(OnClickBtnGameStart);
         btnOption?.onClick.AddListener(OnClickBtnOption);
-        btnShop?.onClick.AddListener(OnClickBtnShop);
         // cur, best 스코어 점수 받아오기
         // 받아온 점수를 UI로 보여주는 메서드
     }
@@ -27,7 +25,6 @@ public class UITitle : UIBase
         base.OnClose();
         btnGameStart?.onClick.RemoveListener(OnClickBtnGameStart);
         btnOption?.onClick.RemoveListener(OnClickBtnOption);
-        btnShop?.onClick.RemoveListener(OnClickBtnShop);
     }
     
     public void OnClickBtnGameStart()
@@ -39,11 +36,5 @@ public class UITitle : UIBase
     {
         // UIOption 을 오픈
         //UIManager.Instance.OpenUI<UIOption>();
-    }
-
-    public void OnClickBtnShop()
-    {
-        // UIShop 을 오픈
-        // 상점 관련 얘기가 없어서 물어봐야 합니다.
     }
 }

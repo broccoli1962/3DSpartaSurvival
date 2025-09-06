98346ff3a4eecc4030e6c9d7526b7a9848f3cf8c
Hira7388 <mine7388@gmail.com>
2025-09-06 21:09:22 +0900

    [Feat] UI Pause 기본 변수 생성

--- Assets/Scripts/UI/UIPause.cs
+++ Assets/Scripts/UI/UIPause.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIPause : UIBase
 {
+    // 능력치 인스펙터 연결
+    [field: Header("능력치 표시 관련")]
+    [field: SerializeField] public TextMeshProUGUI[] statNames { get; private set; } // 인스펙터에서 StatName 1~6 연결
+    [field: SerializeField] public TextMeshProUGUI[] statValues { get; private set; } // 인스펙터에서 StatValue 1~6 연결
+
     // 버튼 인스펙터 연결
+    [field: Header("버튼 연결")]
     [field: SerializeField] public Button btnContinue { get; private set; }
     [field: SerializeField] public Button btnOption { get; private set; }
     [field: SerializeField] public Button btnRestart { get; private set; }
@@ -13,6 +20,7 @@ public class UIPause : UIBase
 
 
     // UISelectItem과 동일함
+    [field: Header("보유 아이템 관련")]
     [field: SerializeField] public Transform ownedItemIconParent { get; private set; } // 하단 보유 아이템 아이콘 위치
     [field: SerializeField] public GameObject ownedItemIconPrefab { get; private set; } // 하단 아이콘 프리팹
 
@@ -26,7 +34,7 @@ public class UIPause : UIBase
         // 획득 아이템 아이콘 업데이트
         UpdateOwnedItem();
         // 능력치 1 업데이트
-        // 능력치 2 업데이트
+        UpdateAbilitiesSectorOne();
     }
 
     protected override void OnClose()
@@ -42,6 +50,12 @@ public class UIPause : UIBase
     // 보유한 아이템을 업데이트하는 메서드
     private void UpdateOwnedItem()
     {
+        // 이전에 생성했던 아이콘 슬롯들을 모두 삭제
+        foreach (Transform child in ownedItemIconParent)
+        {
+            Destroy(child.gameObject);
+        }
+
         // PlayerItemManager로부터 현재 보유한 아이템 목록을 가져옴
         var ownedItems = PlayerItemManager.Instance.ownedItems;
 
